266577fba781320705d54abba93a8af876837436
Nikita Savik <nikita@liveplanet.net>
2019-10-03 16:31:45 +0300

    Hacks for testing

--- profiles/profiles.go
+++ profiles/profiles.go
@@ -34,11 +34,17 @@ func ProfileFromFile(filepath string) (*Profile, error) {
 func (p *Profile) Render(input, output string) string {
 	built := []string{"ffmpeg"}
 
+	if p.Name == "test" {
+		input = "/tmp/in.mp4"
+	}
+
 	built = append(built, "-i "+input)
 
 	for _, c := range p.Spec.Components {
 		built = append(built, c.Render())
 	}
+
+	output += "/index.m3u8"
 
 	built = append(built, output)
 	return strings.Join(built, " ")
