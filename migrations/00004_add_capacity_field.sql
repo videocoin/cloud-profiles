13880ccc96a84800b79607c0a6fce303ccbaca02
cyc60 <avsysoev60@gmail.com>
2020-03-30 12:13:19 +0300

    Fix profiles migration

--- migrations/00004_add_capacity_field.sql
+++ migrations/00004_add_capacity_field.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE capacity ADD `capacity` json DEFAULT NULL;
+ALTER TABLE profiles ADD `capacity` json DEFAULT NULL;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
